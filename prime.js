d7715eac65959cf0baf1dde6a0b22767f7bd113f
Tusar Mondal <tusharmondalbstma@gmail.com>
2022-11-05 16:41:04 +0530

    Code on prime

---
+++ prime.js
@@ -0,0 +1,10 @@
+let prime = (num) => {
+    for (var i = 2; i <= num ** 0.5; i++) {
+      if (num % i == 1)
+        return false;
+    }
+    return true;
+  }
+  
+  let num = 43;
+  prime(num) ? console.log(num, "is prime") : console.log(num, "is not prime");
